cd23866b4cb5db25dcf328c886749346b8a5f475
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-22 00:12:52 -0600

    changes the call to one lambda function

--- src/numbers/logic.js
+++ src/numbers/logic.js
@@ -27,26 +27,7 @@ export const numbersFetchLogic = createLogic({
       let currentState = getState().numbers;
       let prefix = currentState.prefix;
       let category = currentState.category;
-      let numbers;
-      if(prefix.length && category.length) {
-        numbers = await httpClient.post("https://kvcb6yoa1b.execute-api.us-east-1.amazonaws.com/Zed/bycatprefix",{ "startsWith": action.payload, "prefix":prefix, "category":category}).then(resp => resp.data)
-      } else if(prefix.length) {
-        numbers = await httpClient.post("https://et4y5kijs1.execute-api.us-east-1.amazonaws.com/Zed/byprefix",{ "startsWith": action.payload, "prefix":prefix}).then(resp => resp.data)
-      } else if (category.length) {
-        numbers = await httpClient.post("https://xod3dzex23.execute-api.us-east-1.amazonaws.com/zed/bycategory",{ "startsWith": action.payload, "category":category}).then(resp => resp.data)
-      } else {
-        numbers = await httpClient.post("https://gm9gixp04b.execute-api.us-east-1.amazonaws.com/Zed/byname",{ "startsWith": action.payload}).then(resp => resp.data)
-      }
-      //Tried to use a step function but it was too slow, will probably reqwrite a single lambda function to handle this
-      //And use each sepparate function for something else
-      // const numbers = await httpClient.post("https://nvebbpncre.execute-api.us-east-1.amazonaws.com/zed/execution",
-      // {"input": "{\"prefix\": \"\", \"category\": \"Medical\", \"startsWith\": \"S\"}","stateMachineArn":"arn:aws:states:us-east-1:077838313479:stateMachine:GetNumbersStateMachine"})
-      // .then((resp) => {
-      //   await httpClient.post("https://hcfu4wpi09.execute-api.us-east-1.amazonaws.com/zed/execution",{"executionArn": resp.data.executionArn}).then(resp => resp.data);
-      // });
-        // console.log(numbers, 'numbers')
-      // const numbers = await httpClient.post("https://hcfu4wpi09.execute-api.us-east-1.amazonaws.com/zed/execution",{"executionArn": arn}).then(resp => resp.data); // use data property of payload
-      // console.log(numbers, 'numbers')
+      const numbers = await httpClient.post("https://kvcb6yoa1b.execute-api.us-east-1.amazonaws.com/Zed/bycatprefix",{ "startsWith": action.payload, "prefix":prefix, "category":category}).then(resp => resp.data)
       dispatch(numbersFetchFulfilled(numbers));
     } catch(err) {
       console.error(err); // might be a render err
