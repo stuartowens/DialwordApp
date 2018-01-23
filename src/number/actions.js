1c755eeca56d81415b0c035e6be9e7e4c992049a
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-01-23 00:14:53 -0600

    adding user profile fetch

---
+++ src/number/actions.js
@@ -0,0 +1,39 @@
+export const key = 'number';
+
+export const NUMBER_FETCH = 'NUMBER_FETCH';
+export const NUMBER_FETCH_FULFILLED = 'NUMBER_FETCH_FULFILLED';
+export const NUMBER_FETCH_REJECTED = 'NUMBER_FETCH_REJECTED';
+
+export actionTypes = {
+  NUMBER_FETCH,
+  NUMBER_FETCH_FULFILLED,
+  NUMBER_FETCH_REJECTED
+}
+
+export const numberFetch = (name) => (
+  {
+    type: NUMBER_FETCH,
+    payload: name
+  }
+);
+
+export const numberFetchFulfilled = (number) => (
+  {
+    type: NUMBER_FETCH_FULFILLED,
+    payload: number
+  }
+);
+
+export const numberFetchRejected = (err) => (
+  {
+    type: NUMBER_FETCH_REJECTED,
+    payload: err,
+    error: true
+  }
+);
+
+export const actions = {
+  numberFetch,
+  numberFetchFulfilled,
+  numberFetchRejected
+};
