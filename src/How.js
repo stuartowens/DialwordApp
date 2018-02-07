c4285df237ed1c9209ba83d3d957c1785af7c23e
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-02-07 17:59:08 -0600

    adds some styles and content to the How component

--- src/How.js
+++ src/How.js
@@ -6,8 +6,16 @@ class How extends React.Component {
   render() {
     return (
         <div className="How">
-          <h1>How it works</h1>
-          <p>Our premium toll free vanity numbers are available for exclusive licensing by geographic area, which can consist of one area code or multiple states. We work with you to design a custom plan that helps achieve your goals.</p>
+          <h1 className="HPremium">How It Works</h1>
+          <p className="Premium">Our premium toll free vanity numbers are available for exclusive licensing by geographic area, which can consist of one area code or multiple states. We work with you to design a custom plan that helps achieve your goals.</p>
+          <p className="Effective">The option to license a premium number for use in a specific market area makes this a cost effective way to own the rights to a premium toll free vanity number while leaving the number available for similar businesses located in other markets. This allows the creation of a network of businesses like yours, using and advertising the same number throughout the US, giving consumers one memorable contact number for similar products or services.</p>
+          <p className="Example">For example, a chiropractor in Georgia could use 1-800-StopPain at the same time a chiropractor in New York is using it. Their custom call routing plans would deliver the calls from within each specific market area to the correct location. We can tell where a call is originating and route it to the correct location.</p>
+          <p className="Rights">Own the rights to use your toll free vanity number in all of your marketing and advertising campaigns</p>
+          <p className="Area">Receive all the calls that originate from your specific market area</p>
+          <p className="Exclude">Exclude competitors in your specific market area from being able to use your perfect number</p>
+          <p className="Consultants">Have free access to our marketing consultants with years of experience in using vanity numbers in all types of advertising</p>
+          <p className="Participate">Participate in the synergy created by multiple users all advertising the same number across the nation</p>
+          <p className=""></p>
         </div>
       )
   }
