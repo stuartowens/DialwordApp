ab92935ef7c49f1eeb01066347c22a48c1a60fa9
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2018-02-14 20:41:40 -0600

    fixes styles

--- src/How.js
+++ src/How.js
@@ -5,10 +5,7 @@ import React from 'react';
 class How extends React.Component {
   render() {
     return (
-        <div className="How" id="myid">
-          <ul className="list">
-            <li className="listItem">Stu</li>
-          </ul>
+        <div className="How">
           <h1 className="HPremium">How It Works</h1>
           <p className="Premium">Our premium toll free vanity numbers are available for exclusive licensing by geographic area, which can consist of one area code or multiple states. We work with you to design a custom plan that helps achieve your goals.</p>
           <p className="Effective">The option to license a premium number for use in a specific market area makes this a cost effective way to own the rights to a premium toll free vanity number while leaving the number available for similar businesses located in other markets. This allows the creation of a network of businesses like yours, using and advertising the same number throughout the US, giving consumers one memorable contact number for similar products or services.</p>
@@ -18,7 +15,6 @@ class How extends React.Component {
           <p className="Exclude">Exclude competitors in your specific market area from being able to use your perfect number</p>
           <p className="Consultants">Have free access to our marketing consultants with years of experience in using vanity numbers in all types of advertising</p>
           <p className="Participate">Participate in the synergy created by multiple users all advertising the same number across the nation</p>
-          <p className=""></p>
         </div>
       )
   }
